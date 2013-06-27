91a3e83aefbcb21f1e947478ad21e15212c42063
Bernardo Mariano <bernamariano@gmail.com>
2013-06-27 11:52:25 -0300

    Fixing passed/failed count

--- Jayst.js
+++ Jayst.js
@@ -20,20 +20,22 @@
     };
     Jayst.prototype.run = function (file_name) {
         this[file_name].prototype = new Jayst();
-        console.log('================ tests session start ================');
+        console.log('================ Jayst session start ================');
         console.group(file_name);
         console.time('Finished in');
         var dontRun = ['assert', 'run', 'setUp', 'tearDown', 'testsPassed', 'testsFailed'];
-        for (Jayst in (new this[file_name])) {
-            if (dontRun.indexOf(Jayst) < 0) {
+        for (test in (new this[file_name])) {
+            if (dontRun.indexOf(test) < 0) {
                 try { new this[file_name]().setUp(); } catch (e) {};
-                new this[file_name]()[Jayst]();
+                new this[file_name]()[test]();
                 try { new this[file_name]().tearDown(); } catch (e) {};
             }
         }
         console.timeEnd('Finished in');
         console.groupEnd();
         console.log('================ ' + Jayst.testsFailed + ' failed, ' + Jayst.testsPassed + ' passed ================');
+        Jayst.testsPassed = 0;
+        Jayst.testsFailed = 0;
     }
     window.Jayst = Jayst;
 }(window));
